a4a5d7440b80d116b1c3213a6bdbcd02fecc9847
Dark-wizZ <mailwithakram@gmail.com>
2024-01-12 14:56:01 +0530

    04 Pass test 01

--- sem6/com_dis/lab/exes/04.py
+++ sem6/com_dis/lab/exes/04.py
@@ -1,3 +1,4 @@
+
 e=u'\u03b5'
 p=[]
 
@@ -14,45 +15,49 @@ class Prod:
     print(s)
     
 def trans():    
-    #for x in p:
-        x = p[0]
-        temp = x.products
-        temp.sort()
-        x.prdoucts=[]
-        for i in range(len(temp)):
-            temp[i] = f'{temp[i]}{e}'
-        while temp:
-            group=[]
-            alpha=''
-            beta=[]
-            for i in range(1,len(temp)):
-                if (temp[0][0] == temp[i][0]):
-                    group.append(temp[i])
-            if group:
-                group.insert(0,temp[0])
-                temp = [y for y in temp if y not in group]
+    a = p[0]
+    temp = a.products
+    temp.sort()
+    a.products=[]
+    while(temp):
+        group=[]
+        alpha=''
+        beta=[]
+        for i in range(1, len(temp)):
+            if(temp[0][0] == temp[i][0]):
+                group.append(temp[i])
+        if group:
+            group.insert(0, temp[0])
+            temp = [j for j in temp if j not in group]
+            # add epsilon to the grouped production
+            for j in range(len(group)):
+                group[j]+=e
+            for c in group[0]:
                 f1=0
-                for c in group[0]:
-                    for j in range(1, len(group)):
-                        if(group[j][0] != c):
-                            f1=1
-                    if f1:
-                        p.append(Prod(f"{alpha[0]}'",group))
-                    else:
-                        alpha += c
-                        for j in range(1, len(group)):
-                            group[j] = group[j][1:]
+                for j in group:
+                    if c != j[0]:
+                        f1=1
+                if f1:
+                    beta=group
+                    break
+                else:
+                    alpha+=group[0][0]
+                    for j in range(len(group)):
+                        group[j] = group[j][1:]
+            # remove epsilons
+            for j in range(len(beta)):
+                if(beta[j][0]!=e):
+                    beta[j]=beta[j][:-1]
+            a.products.append(alpha+alpha[0]+"'")
+            p.append(Prod(alpha[0]+"'",beta))
+        else:
+            a.products.append(temp[0])
+            temp.pop(0)
 
-            else:
-                x.products.append(temp[0])
-                temp.pop(0) 
-
-n = int(input("No of production: "))
-for i in range(n):
-    ip = input(f"Production {i+1}: ")
-    name, prods = ip.split(' -> ')
-    products = prods.split(' | ')
-    p.append(Prod(name, products))
+ip = input(f"Enter production: ")
+name, prods = ip.split(' -> ')
+products = prods.split(' | ')
+p.append(Prod(name, products))
   
 print('Productions:')
 for x in p: x.print()
