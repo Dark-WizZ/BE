9414d153cf67d0e4d4475f5001cd5abe38aebea0
Akram <102450631+Dark-wizZ@users.noreply.github.com>
2024-04-02 14:58:03 +0530

    Update 07.py

--- sem6/com_dis/lab/exes/07.py
+++ sem6/com_dis/lab/exes/07.py
@@ -1,24 +1,33 @@
 gram = {
-    "S": ["S+S", "S*S",'S-S','(S)', "id"]
+    "S":  ["S+S", "S*S",'S-S','(S)', "id"]
 }
 start = "S"
 inp = "id+id-(id+id)$"
 stack = "$"
-print(f'{"Stack": <15}' + "|" + f'{"Input Buffer": <15}' + "|" + 'Parsing Action')
+
+print(f'{"Stack": <15}' + "|" + f'{"Input Buffer": <15}' + "|" + f'Parsing Action')
 print(f'{"-":-<50}')
+
 while True:
+    flag = 1
     i = 0
-    for i in range (len(gram[start])):
+
+    for i in range(len(gram[start])):
         if gram[start][i] in stack:
             stack = stack.replace(gram[start][i], start)
-            print(f'{stack: <15}' + "|" + f'{inp: <15}' + "|" + f'Reduce > {gram[start][i]}')
+            print(f'{stack: <15}' + "|" + f'{inp: <15}' + "|" + f'Reduce S->{gram[start][i]}')
+            flag = 0
+
     if len(inp) > 1:
         stack += inp[0]
         inp = inp[1:]
-        print(f'{stack: <15}' + "|" + f'{inp: <15}' + "|" + 'Shift')
-    if stack == ("$" + start):
-        if inp == '$':
-            print(f'{stack: <15}' + "|" + f'{inp: <15}' + "|" + 'Accepted')
-        else:
-            print(f'{stack: <15}' + "|" + f'{inp: <15}' + "|" + 'Rejected')
+        print(f'{stack: <15}' + "|" + f'{inp: <15}' + "|" + f'Shift')
+        flag = 0
+
+    if inp == "$" and stack == ("$" + start):
+        print(f'{stack: <15}' + "|" + f'{inp: <15}' + "|" + f'Accepted')
+        break
+
+    if flag:
+        print(f'{stack: <15}' + "|" + f'{inp: <15}' + "|" + f'Rejected')
         break
